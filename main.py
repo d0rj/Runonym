493d3e788fe11cdf06af07120a4d07008bf21fbb
d0rj <hanaan@internet.ru>
2021-01-20 21:47:14 +0300

    Synonym search functions splitted into unique functions.

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import sys
 import os
-from nltk.stem.snowball import SnowballStemmer 
 
 
 def error(message: str) -> None:
@@ -8,18 +7,40 @@ def error(message: str) -> None:
 	exit(1)
 
 
-def main() -> None:
-	stemmer = SnowballStemmer("russian") 
-
-	if len(sys.argv) < 2:
-		error('No input word')
-	
-	word = sys.argv[1].lower()
-	stemmed_word = stemmer.stem(word)
+def find_strict(word: str) -> bool:
+	"""
+	Searches synonym for given word literally
+	"""
 
 	filepath = './letters/{}.txt'.format(word[0])
+
 	if not os.path.exists(filepath):
 		error('No such synonyms')
+
+	with open(filepath, 'r+', encoding='utf8') as file:
+		for string in file:	
+			if string.lower().startswith(word):
+				result = string.lower().replace('{},'.format(word), '')
+				print(result)
+				return True
+
+	return False
+
+
+def find_started(word: str) -> bool:
+	"""
+	Searches synonyms for all words started with given but lemmatized
+	"""
+
+	from nltk.stem.snowball import SnowballStemmer 
+
+	filepath = './letters/{}.txt'.format(word[0])
+
+	if not os.path.exists(filepath):
+		error('No such synonyms')
+
+	stemmer = SnowballStemmer("russian") 
+	stemmed_word = stemmer.stem(word)
 
 	finded = False
 	with open(filepath, 'r+', encoding='utf8') as file:
@@ -29,6 +50,16 @@ def main() -> None:
 				print(result)
 				finded = True
 
+	return finded
+
+
+def main() -> None:
+	if len(sys.argv) < 2:
+		error('No input word')
+	
+	word = sys.argv[1].lower()
+
+	finded = find_strict(word)
 	if not finded:
 		error('Not finded')
 
